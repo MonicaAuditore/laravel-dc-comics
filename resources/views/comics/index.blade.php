7179c36ca84a302ee64d7c43b0b99f43c233e24c
MonicaAuditore <mauditore@live.it>
2023-03-15 15:40:36 +0100

    add_card in pg comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,9 +5,25 @@
 <div class="container">
     <div class="row">
         <div class="col text-center">
-          <h1 class="card-title">Comics.index</h1>
+          <h1 class="card-title pb-5 pt-5">Tutti i fumetti</h1>
         </div>
     </div>
+    <div class="row g-3">
+      @foreach ($comics as $comic)
+        <div class="col-4 mb-3">
+         <div class="card text-center">
+          <div class="card-body">
+            <h2 class="card-title">{{ $comic->title }}</h2>
+            {{-- <a href="{{ route('comics.show') }}" class="btn btn-primary mt-5 mb-5">Vedi dettagli</a> --}}
+
+          </div>
+         </div>
+        </div>
+      @endforeach
+    </div>
+  </div>
+
+
 </div>
 
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,9 @@
 <div class="container">
     <div class="row">
         <div class="col text-center">
-            <div class="card">
-                <div class="card-body">
-                    <h1>Comics</h1>
-                </div>
-            </div>
+            
+                  <h1 class="card-title">Comics</h1>
+             
             <a href="{{ route('comics.index') }}" class="btn btn-primary mt-5 mb-5">Vedi tutti i fumetti</a>
         </div>
     </div>
