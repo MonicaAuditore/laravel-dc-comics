444bb29016d8ac9b0c5dc979ab2a7fba73268ce5
MonicaAuditore <mauditore@live.it>
2023-03-15 16:31:49 +0100

    add_form in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -5,8 +5,44 @@
 <div class="container">
     <div class="row">
         <div class="col text-center">
-          <h1 class="card-title">Comics.create</h1>
+          <h1 class="card-title">Aggiungi un fumetto</h1>
+          <a href="{{ route('comics.index') }}" class="btn btn-primary mt-5 mb-5">Torna indietro</a>
         </div>
+        <form action="{{ route('comics.store') }}" method="POST">
+          @csrf
+
+          <div class="mb-3">
+            <label for="title" class="form-label">Titolo</label>
+            <input type="text" class="form-control" name="title" id="title" placeholder="Inserisci il titolo...">
+          </div>
+          <div class="mb-3">
+            <label for="description" class="form-label">Descrizione</label>
+            <textarea class="form-control" name="description" id="description" rows="3" placeholder="Inserisci la descrizione..."></textarea>
+          </div>
+          <div class="mb-3">
+            <label for="thumb" class="form-label">Immagine</label>
+            <input type="text" class="form-control" name="thumb" id="thumb" placeholder="Inserisci url dell'immagine...">
+          </div>
+          <div class="mb-3">
+            <label for="price" class="form-label">Prezzo</label>
+            <input type="number" class="form-control" name="price" id="price" placeholder="Inserisci il prezzo...">
+          </div>
+          <div class="mb-3">
+            <label for="series" class="form-label">Serie</label>
+            <input type="text" class="form-control" name="series" id="series" placeholder="Inserisci la serie...">
+          </div>
+          <div class="mb-3">
+            <label for="sale_date" class="form-label">Data di vendita</label>
+            <input type="date" class="form-control" name="sale_date" id="sale_date" placeholder="Inserisci la data di vendita...">
+          </div>
+          <div class="mb-3">
+            <label for="type" class="form-label">Tipo</label>
+            <input type="text" class="form-control" name="type" id="type" placeholder="Inserisci il tipo...">
+          </div>
+          <button class="btn btn-primary" type="submit">
+            Salva
+          </button>
+        </form>
     </div>
 </div>
 
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,7 +5,9 @@
 <div class="container">
     <div class="row">
         <div class="col text-center">
-          <h1 class="card-title pb-5 pt-5">Tutti i fumetti</h1>
+          <h1 class="card-title pt-3">Tutti i fumetti</h1>
+          <a href="{{ route('comics.create') }}" class="btn btn-primary mt-5 mb-5">Aggiungi un fumetto</a>
+
         </div>
     </div>
     <div class="row g-3">
